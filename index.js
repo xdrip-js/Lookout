3307ee993761ed48a70f262b969bbe42f0996e54
Paul Dickens <dickens.paul@gmail.com>
2017-11-12 11:58:06 +1100

    decouple app from xdrip-js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const Transmitter = require('./lib/transmitter');
+#!/usr/bin/env node
 
-module.exports = Transmitter;
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+
+const server = express()
+  .use(express.static(__dirname + '/public'))
+  // note: the '/../node_modules' bit is because the app
+  // doesn't have its own packge.json file
+  // could be an idea to separate the app out as a separate repo
+  .use('/node_modules', express.static(__dirname + '/../node_modules'))
+  .use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css')) // redirect CSS bootstrap
+  // prevent error message on reloads as per https://stackoverflow.com/a/35284602
+  .get('/*', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+const argv = require('yargs').argv;
+const TransmitterIO = argv.sim ? require('./transmitterIO-simulated') : require('./transmitterIO')
+
+TransmitterIO(io);

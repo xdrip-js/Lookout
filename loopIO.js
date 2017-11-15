b84a3bcd4fd5278923a262d4989063efd1f01ca8
Paul Dickens <dickens.paul@gmail.com>
2017-11-15 22:27:51 +1100

    add event name

--- loopIO.js
+++ loopIO.js
@@ -2,6 +2,6 @@ module.exports = (io) => {
   // TODO: use namespaces (cgm, loop, pump) to logically separate msgs
   io.on('connection', socket => {
     iob = require('/root/myopenaps/monitor/iob.json');
-    socket.emit(iob[0]['iob']);
+    socket.emit('iob', iob[0]['iob']);
   });
 };

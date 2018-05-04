813e463b3d0757cd55c41824d9deaaf63fcc1bab
Jeremy Cunningham <jpcunningh@gmail.com>
2018-05-03 22:56:45 -0500

    Implemented syncLSRCalData.

--- xDripAPS.js
+++ xDripAPS.js
@@ -374,6 +374,42 @@ module.exports = () => {
       });
     },
 
+    updateBGCheck: (id, BGCheck) => {
+      let entry = convertBGCheck(BGCheck);
+
+      entry._id = id;
+
+      const secret = process.env.API_SECRET;
+      let ns_url = process.env.NIGHTSCOUT_HOST + '/api/v1/treatments/';
+      let ns_headers = {
+        'Content-Type': 'application/json'
+      };
+
+      if (secret.startsWith('token=')) {
+        ns_url = ns_url + '?' + secret;
+      } else {
+        ns_headers['API-SECRET'] = secret;
+      }
+
+      const optionsNS = {
+        url: ns_url,
+        method: 'PUT',
+        headers: ns_headers,
+        data: entry,
+        json: true
+      };
+
+      /*eslint-disable no-unused-vars*/
+      request(optionsNS, function (error, response, body) {
+      /*eslint-enable no-unused-vars*/
+        if (error) {
+          console.error('error posting json: ', error);
+        } else {
+          console.log('updated BG Check to NS, statusCode = ' + response.statusCode);
+        }
+      });
+    },
+
     postBGCheck: (BGCheck) => {
       let entry = convertBGCheck(BGCheck);
 

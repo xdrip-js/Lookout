14867997820df48c1e3e1d09a2711b44e0e49ca9
Jeremy Cunningham <jpcunningh@gmail.com>
2018-10-06 17:24:56 -0500

    Added prompt.js module.

---
+++ prompt.js
@@ -0,0 +1,13 @@
+
+module.exports = (question) => {
+  return new Promise((resolve, reject) => {
+    const { stdin, stdout } = process;
+
+    stdin.resume();
+    stdout.write(question);
+
+    stdin.on('data', data => resolve(data.toString().trim()));
+    stdin.on('error', err => reject(err));
+  });
+}
+

08bb1f0c2cf1b1b0a17e26875dbff3039704e2a9
Jeremy Cunningham <jpcunningh@gmail.com>
2018-10-16 18:31:50 -0500

    Added route for meterid

--- public/cgm/transmitter/transmitter.js
+++ public/cgm/transmitter/transmitter.js
@@ -11,6 +11,10 @@ angular.module('AngularOpenAPS.cgm.transmitter', [
       templateUrl: 'cgm/transmitter/pair.html',
       controller: 'TransmitterController'
     });
+    $routeProvider.when('/cgm/transmitter/meterid', {
+      templateUrl: 'cgm/transmitter/meterid.html',
+      controller: 'TransmitterController'
+    });
     $routeProvider.when('/cgm/transmitter/reset', {
       templateUrl: 'cgm/transmitter/reset.html',
       controller: 'TransmitterController'

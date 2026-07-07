3c95890732fc0b44cba6a25554a097fa48723435
Jeremy Cunningham <jpcunningh@gmail.com>
2026-07-06 20:25:22 -0500

    update debug configuration

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ const init = async () => {
     } else if (options.verbose === 1) {
       Debug.enable(lookoutDebug);
     } else if (options.verbose === 2) {
-      Debug.enable(`${lookoutDebug},signaling,bindings,acl-att-stream,att,gap`);
+      Debug.enable(`${lookoutDebug},keks-calc,signaling,bindings,acl-att-stream,att,gap`);
     } else {
       Debug.enable('*,*:*');
     }

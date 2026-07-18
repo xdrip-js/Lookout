ef045dcda6d4682bb331bb70d5cd62c52c3a6e7b
Jeremy Cunningham <jpcunningh@gmail.com>
2026-07-17 19:43:09 -0500

    fix starting worker with no key or mac

--- transmitterWorker.js
+++ transmitterWorker.js
@@ -10,8 +10,8 @@ const Transmitter = require('xdrip-js');
 
 const id = process.argv[2];
 const altBtChannel = parseInt(process.argv[3], 10);
-const key = process.argv[4].length > 0 ? process.argv[4] : null;
-const macAddress = process.argv[5].length > 0 ? process.argv[5] : null;
+const key = process.argv[4]?.trim() || null;
+const macAddress = process.argv[5]?.trim() || null;
 
 /* eslint-disable-next-line no-unused-vars */
 const getMessages = () => new Promise((resolve, reject) => {
